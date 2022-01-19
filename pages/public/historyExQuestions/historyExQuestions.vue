e47d4ab06e7d8f7cefe90094f3aa117d344a2f9b
liqinfei <526700468@qq.com>
2022-01-19 22:54:22 +0800

    历史考题开发

--- pages/public/historyExQuestions/historyExQuestions.vue
+++ pages/public/historyExQuestions/historyExQuestions.vue
@@ -29,18 +29,61 @@
 					主办方与选中侧边栏二级菜单一致
 				</view>
 				<view class="date">
-					<view class="date-list">
+				<!-- 	<view class="date-list">
 						<view class="date-item" v-for="item in 20">
 							2020
 						</view>
-					</view>
-					
+					</view> -->
+					<drawingColumn></drawingColumn>
 				</view>
-				
-				<view class="r-tab">
+
+				<scroll-view class="r-tab" :scroll-x="true">
 					<view class="tab-list">
-						<view class="tab-item" v-for="item in rightTabList" :key="item.id">
+						<view class="tab-item" @click="handleRTab(item)" :class="rightTabNum === item.id ? 'tab-item-active':''" v-for="item in rightTabList" :key="item.id">
 							{{ item.name }}
+						</view>
+					</view>
+				</scroll-view>
+				
+				<view class="r-content">
+					<view class="test-list">
+						<view class="test-name">考试名称</view>
+						<view class="subjects-item">
+							<view class="subjects-name">
+								科目
+							</view>
+							<view class="item">
+								<view class="name">
+									考试题目
+								</view>
+								<view class="item-b">
+									<view class="text">
+										人物半身像随意创作性别年龄大小种族不限，西方人物室外风景色彩赏析，试的师傅说
+									</view>
+									<view class="time">
+										<view class="t-l">
+											<view class="l">考试时间</view>
+											<view class="r">2019.12.26</view>
+										</view>
+										<view class="t-r">
+											查看详情<u-icon name="arrow-right" size="24" class="arrow"></u-icon>
+										</view>
+									 </view>
+									 <view class="line"></view>
+									 <view class="bottom">
+									 	<view class="left">
+									 		<image class="img" src="../../../static/public/examinationPaper_icon.png" mode=""></image>
+											高分试卷
+									 	</view>
+										<view class="right">
+											<image class="route" src="../../../static/public/route.png" mode=""></image>
+										</view>
+									 </view>
+								</view>
+								
+								
+								
+							</view>
 						</view>
 					</view>
 				</view>
@@ -50,7 +93,11 @@
 </template>
 
 <script>
+	import drawingColumn from '../../../components/drawingColumn/drawingColumn.vue'
 	export default {
+		components:{
+			drawingColumn	
+		},
 		data() {
 			return {
 				tabNumber: 'one',
@@ -60,7 +107,8 @@
 				],
 				leftNum: 0,
 				leftItemNum: '1-1',
-				rightTabList: [{id: 1,name: '设计师'},{id: 2,name: '产品设计'},{id: 3,name: '环境设计'},{id: 4,name: '平面设计'}]
+				rightTabList: [{id: 1,name: '设计师'},{id: 2,name: '产品设计'},{id: 3,name: '环境设计'},{id: 4,name: '平面设计'}],
+				rightTabNum: 1
 			}
 		},
 		methods: {
@@ -72,6 +120,9 @@
 			},
 			handleItemListClick(item){
 				this.leftItemNum = item.id
+			},
+			handleRTab(item){
+				this.rightTabNum = item.id
 			}
 		}
 	}
@@ -208,18 +259,142 @@
 				
 				.r-tab{
 					// display: flex;
-					overflow-x: scroll;
+					// overflow-x: scroll;
 					width: 100%;
 					padding-left: 16rpx;
-					padding-bottom: 20rpx;
+					padding-bottom: 16rpx;
 					.tab-list{
 						white-space: nowrap;
 						.tab-item{
 							display: inline-block;
 							margin-right: 50rpx;
 						}
+						.tab-item-active{
+							color: #2C3AFF;
+						}
 					}
 					border-bottom: 2rpx solid #E9E9E9;
+				}
+				.r-content{
+					padding: 16rpx 34rpx 0 16rpx;
+					.test-list{
+						.test-name{
+							text-align: center;
+							font-size: 30rpx;
+							font-family: PingFangSC-Regular, PingFang SC;
+							font-weight: 400;
+							color: #3A3D71;
+							line-height: 30rpx
+						}
+						.subjects-item{
+							.subjects-name{
+								text-align: center;
+								font-size: 34rpx;
+								font-family: PingFang-SC-Bold, PingFang-SC;
+								font-weight: bold;
+								color: #3A3D71;
+								line-height: 34rpx;
+								margin: 16rpx 0;
+							}
+							.item{
+								// width: 526px;
+								height: 314rpx;
+								background: #FFFFFF;
+								box-shadow: 0px 6rpx 16rpx 6rpx rgba(230, 230, 230, 0.5);
+								border-radius: 16rpx;
+								.name{
+									width: 100%;
+									height: 56rpx;
+									background: linear-gradient(90deg, #FFFFFF 0%, #EFF2FF 100%);
+									border-radius: 16rpx 16rpx 0rpx 0rpx;
+									font-size: 28rpx;
+									font-family: PingFangSC-Regular, PingFang SC;
+									font-weight: 400;
+									color: #3A3D71;
+									line-height: 56rpx;
+									padding-left: 24rpx
+								}
+								.item-b{
+									padding: 0 24rpx 0 24rpx;
+								}
+								.text{
+									margin-top: 14rpx;
+									font-size: 26rpx;
+									font-family: PingFang-SC-Regular, PingFang-SC;
+									font-weight: 400;
+									color: #3A3D71;
+									line-height: 36rpx;
+									 text-overflow: -o-ellipsis-lastline;
+									  overflow: hidden;
+									  text-overflow: ellipsis;
+									  display: -webkit-box;
+									  -webkit-line-clamp: 2;
+									  line-clamp: 2;
+									  -webkit-box-orient: vertical;
+								}
+								.time{
+									margin-top: 12rpx;
+									display: flex;
+									.t-l{
+										flex: 1;
+										.l{
+											display: inline;
+											font-size: 24rpx;
+											font-family: PingFang-SC-Regular, PingFang-SC;
+											font-weight: 400;
+											color: #888C90;
+										}
+										.r{
+											margin-left: 20rpx;
+											display: inline;
+											font-size: 24rpx;
+											font-family: PingFangSC-Regular, PingFang SC;
+											font-weight: 400;
+											color: #3A3D71;
+											line-height: 24rpx;
+										}
+									}
+									.t-r{
+										flex: 1;
+										text-align: right;
+										font-size: 24rpx;
+										font-family: PingFang-SC-Regular, PingFang-SC;
+										font-weight: 400;
+										color: #9E9E9E;
+									}
+								}
+								.line{
+									width: 100%;
+									height: 2rpx;
+									background: #E9E9E9;
+									margin-top: 26rpx;
+									margin-bottom: 20rpx;
+								}
+								.bottom{
+									width: 100%;
+									display: flex;
+									// background: #DCDFE6;
+									.left{
+										flex: 1;
+										.img{
+											width: 44rpx;
+											height: 44rpx;
+											vertical-align: bottom;
+											margin-right: 24rpx;
+										}
+									}
+									.right{
+										text-align: right;
+										flex: 1;
+										.route{
+											width: 34rpx;
+											height: 14rpx;
+										}
+									}
+								}
+							}
+						}
+					}
 				}
 			}
 		}
