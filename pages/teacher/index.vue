7f30f116b56458dca23b8844a32d18de4c52f26a
fengyuxuan <fengyuxuan>
2022-02-09 10:30:20 +0800

    【描述】：提交

--- pages/teacher/index.vue
+++ pages/teacher/index.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
 	<view>
 		<tab-bar :selected="3"></tab-bar>
 	</view>
@@ -20,4 +20,376 @@
 
 <style lang="scss">
 
+</style> -->
+
+<template>
+	<view class="u-wrap">
+		<view class="u-menu-wrap">
+			<scroll-view scroll-y scroll-with-animation class="u-tab-view menu-scroll-view" :scroll-top="scrollTop"
+				:scroll-into-view="itemId">
+				<view v-for="(item,index) in tabbar" :key="index" class="u-tab-item"
+					:class="[current == index ? 'u-tab-item-active' : '']" @tap.stop="swichMenu(item, index)">
+					<text class="u-line-1">{{item.name}}</text>
+				</view>
+			</scroll-view>
+			<view class="right-box">
+				<!-- option -->
+				<view class="opt-container">
+					<drawingColumn ref="DrawingColumn" @change="tabChange"></drawingColumn>
+				</view>
+				<!-- option end-->
+				<scroll-view scroll-y class="right-box-scroll">
+					<view class="page-view">
+						5555
+						<view class="class-item" v-for="(item , index) in list" :key="index" @click="detailTap(item)">
+							<view class="item-container">
+								<famous-teacher-item :item="item"></famous-teacher-item>
+							</view>
+					</view>
+					</view>
+				</scroll-view>
+			</view>
+			
+		</view>
+	</view>
+</template>
+<script>
+	
+	import { classify, schoolList } from '@/api/school.js';
+	import drawingColumn from '@/components/drawingColumn/drawingColumn.vue';
+	import FamousTeacherItem from '@/components/famousTeacher/famousTeacherItem.vue';
+	export default {
+		components:{
+			drawingColumn,
+			FamousTeacherItem
+		},
+		data() {
+			return {
+				scrollTop: 0, //tab标题的滚动条位置
+				oldScrollTop: 0,
+				current: 0, // 预设当前项的值
+				menuHeight: 0, // 左边菜单的高度
+				menuItemHeight: 0, // 左边菜单item的高度
+				itemId: '', // 栏目右边scroll-view用于滚动的id
+				tabbar: [],
+				list: [],
+				classifyId: '',
+				provinceId: '',
+				menuItemPos: [],
+				arr: [],
+				scrollRightTop: 0, // 右边栏目scroll-view的滚动条高度
+				timer: null, // 定时器
+				
+				//筛选选项
+				optionList:[
+					{
+						value:'1',
+						name:'全部'
+					},
+					{
+						value:'2',
+						name:'色彩'
+					},
+					{
+						value:'3',
+						name:'素描'
+					},
+					{
+						value:'4',
+						name:'速写'
+					},
+					{
+						value:'6',
+						name:'其他'
+					}
+				],
+				
+			}
+		},
+		onLoad() {
+			this.initData()
+		},
+		onReady() {
+			this.getMenuItemTop()
+		},
+		methods: {
+			initData(){
+				this.$http.get(classify).then(res => {
+					console.log(res)
+					this.tabbar = res.data
+					if(this.tabbar.length){
+						this.classifyId = this.tabbar[0].classify_id;
+						this.provinceId = this.tabbar[0].province_id;
+						this.getSchoolList()
+					}
+				})
+			},
+			getSchoolList(){
+				this.$http.post(schoolList, {
+					classifyId: this.classifyId,
+					provinceId: this.provinceId,
+					size: 100
+				}).then(res => {
+					this.list = res.data.records
+				})
+			},
+			detailTap(item){
+				console.log(item)
+				this.$mRouter.push({
+					route: `/pages/teachers/dynamicHomePage?universityId=${item.id}`
+				})
+			},
+			// 点击左边的栏目切换
+			async swichMenu(item, index) {
+				console.log(item)
+				this.current = index;
+				this.classifyId = item.classify_id;
+				this.getSchoolList();
+				// if (this.arr.length == 0) {
+				// 	await this.getMenuItemTop();
+				// }
+				// if (index == this.current) return;
+				// this.scrollRightTop = this.oldScrollTop;
+				// this.$nextTick(function() {
+				// 	this.scrollRightTop = this.arr[index];
+				// 	this.current = index;
+				// 	this.leftMenuStatus(index);
+				// })
+			},
+			// 获取一个目标元素的高度
+			getElRect(elClass, dataVal) {
+				new Promise((resolve, reject) => {
+					const query = uni.createSelectorQuery().in(this);
+					query.select('.' + elClass).fields({
+						size: true
+					}, res => {
+						// 如果节点尚未生成，res值为null，循环调用执行
+						if (!res) {
+							setTimeout(() => {
+								this.getElRect(elClass);
+							}, 10);
+							return;
+						}
+						this[dataVal] = res.height;
+						resolve();
+					}).exec();
+				})
+			},
+			// 观测元素相交状态
+			async observer() {
+				this.tabbar.map((val, index) => {
+					let observer = uni.createIntersectionObserver(this);
+					// 检测右边scroll-view的id为itemxx的元素与right-box的相交状态
+					// 如果跟.right-box底部相交，就动态设置左边栏目的活动状态
+					observer.relativeTo('.right-box', {
+						top: 0
+					}).observe('#item' + index, res => {
+						if (res.intersectionRatio > 0) {
+							let id = res.id.substring(4);
+							this.leftMenuStatus(id);
+						}
+					})
+				})
+			},
+			// 设置左边菜单的滚动状态
+			async leftMenuStatus(index) {
+				this.current = index;
+				// 如果为0，意味着尚未初始化
+				if (this.menuHeight == 0 || this.menuItemHeight == 0) {
+					await this.getElRect('menu-scroll-view', 'menuHeight');
+					await this.getElRect('u-tab-item', 'menuItemHeight');
+				}
+				// 将菜单活动item垂直居中
+				this.scrollTop = index * this.menuItemHeight + this.menuItemHeight / 2 - this.menuHeight / 2;
+			},
+			// 获取右边菜单每个item到顶部的距离
+			getMenuItemTop() {
+				new Promise(resolve => {
+					let selectorQuery = uni.createSelectorQuery();
+					selectorQuery.selectAll('.class-item').boundingClientRect((rects) => {
+						// 如果节点尚未生成，rects值为[](因为用selectAll，所以返回的是数组)，循环调用执行
+						if (!rects.length) {
+							setTimeout(() => {
+								this.getMenuItemTop();
+							}, 10);
+							return;
+						}
+						rects.forEach((rect) => {
+							// 这里减去rects[0].top，是因为第一项顶部可能不是贴到导航栏(比如有个搜索框的情况)
+							this.arr.push(rect.top - rects[0].top);
+							resolve();
+						})
+					}).exec()
+				})
+			},
+			// 右边菜单滚动
+			async rightScroll(e) {
+				// this.oldScrollTop = e.detail.scrollTop;
+				// if (this.arr.length == 0) {
+				// 	await this.getMenuItemTop();
+				// }
+				// if (this.timer) return;
+				// if (!this.menuHeight) {
+				// 	await this.getElRect('menu-scroll-view', 'menuHeight');
+				// }
+				// setTimeout(() => { // 节流
+				// 	this.timer = null;
+				// 	// scrollHeight为右边菜单垂直中点位置
+				// 	let scrollHeight = e.detail.scrollTop + this.menuHeight / 2;
+				// 	for (let i = 0; i < this.arr.length; i++) {
+				// 		let height1 = this.arr[i];
+				// 		let height2 = this.arr[i + 1];
+				// 		// 如果不存在height2，意味着数据循环已经到了最后一个，设置左边菜单为最后一项即可
+				// 		if (!height2 || scrollHeight >= height1 && scrollHeight < height2) {
+				// 			this.leftMenuStatus(i);
+				// 			return;
+				// 		}
+				// 	}
+				// }, 10)
+			},
+			// 筛选
+			tabChange(index) {
+				console.log()
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	// option-item
+	.opt-container{
+		background: #FFFFFF;
+		height: 114rpx;
+		padding-top: 30rpx;
+		border-bottom: 2rpx solid #E9E9E9;
+	}
+	// option-item emd
+	.u-wrap {
+		height: calc(100vh);
+		display: flex;
+		flex-direction: column;
+	}
+
+
+	.u-menu-wrap {
+		border-top: 2rpx solid #E9E9E9;
+		flex: 1;
+		display: flex;
+		overflow: hidden;
+	}
+
+	.u-search-inner {
+		background-color: rgb(234, 234, 234);
+		border-radius: 100rpx;
+		display: flex;
+		align-items: center;
+		padding: 10rpx 16rpx;
+	}
+
+	.u-search-text {
+		font-size: 26rpx;
+		color: $u-tips-color;
+		margin-left: 10rpx;
+	}
+
+	.u-tab-view {
+		width: 166rpx;
+		height: 100%;
+		background-color: #F2F2F2;
+	}
+
+	.u-tab-item {
+		height: 116rpx;
+		box-sizing: border-box;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-size: 28rpx;
+		color: #9E9E9E;
+	}
+
+	.u-tab-item-active {
+		position: relative;
+		font-size: 28rpx;
+		font-weight: bold;
+		color: $u-type-primary;
+		background: #fff;
+		border-radius: 24rpx 0px 0px 24rpx;
+	}
+
+	.u-tab-item-active::before {
+		content: "";
+		position: absolute;
+		background-color: $u-type-primary;
+		width: 10rpx;
+		height: 44rpx;
+		border-radius: 0px 6rpx 6rpx 0px;
+		left: 0;
+		top: 39rpx;
+	}
+
+	.u-tab-view {
+		height: 100%;
+	}
+
+	.right-box {
+		flex: 1;
+		background-color: rgb(250, 250, 250);
+		
+		&-scroll{
+			height: 100%;
+		}
+	}
+
+	.page-view {
+		// padding: 16rpx;
+	}
+
+	.item-title {
+		font-size: 26rpx;
+		color: $u-main-color;
+		font-weight: bold;
+	}
+
+
+	.item-container {
+		// display: flex;
+	}
+
+	.thumb-box {
+		padding-top: 28rpx;
+	}
+
+
+	.left {
+		width: 120rpx;
+		height: 120rpx;
+		margin-bottom: 30rpx;
+		border-radius: 16rpx;
+		border: 2rpx solid #E9E9E9;
+
+		.item-menu-image {
+			width: 110rpx;
+			height: 110rpx;
+		}
+	}
+
+	.right {
+		flex: 1;
+		margin: 0 34rpx 0 28rpx;
+		padding-bottom: 30rpx;
+		border-bottom: 2rpx solid #E9E9E9;
+
+		.item-menu-name {
+			font-size: 28rpx;
+			font-weight: bold;
+			color: #3A3D71;
+		}
+
+		.item-menu-dec {
+			font-size: 26rpx;
+			color: #3A3D71;
+		}
+
+	}
 </style>
---
+++ pages/teachers/dynamicHomePage.vue
@@ -0,0 +1,155 @@
+<template>
+	<view class="dynamic-container">
+		<view class="dynamic-header">
+			<view class="">
+				<image class="dynamic-header-back" src="../../static/public/teacherDynamic/d_icon_back.png" mode=""></image>
+			</view>
+			<view class="">
+				<image class="dynamic-header-fx" src="../../static/public/teacherDynamic/d_icon_fx.png" mode=""></image>
+			</view>
+		</view>
+		<view class="dynamic-main">
+			<view class="main-top main-item">
+				<view class="main-top-item header-img">
+					<image class="headerimg" src="../../static/login_wechat.png" mode=""></image>
+					<image class="header-auth" src="../../static/public/auth.png" mode="" v-show="status == '1'"></image>
+				</view>
+				<view class="main-top-item">
+					<view class="text">关注</view>
+					<view class="num">652</view>
+				</view>
+				<view class="main-top-item ">
+					<view class="text">粉丝</view>
+					<view class="num">865w</view>
+				</view>
+				<view class="gz">已关注</view>
+			</view>
+			<view class="person-name main-item">刘泉海·LQH</view>
+			<view class="lable main-item">
+				<view v-for="(item,index) in lableList">{{item}}</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				status:1,
+				// 标签
+				lableList:['色彩','素描','速写']
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+.dynamic-header{
+	background: #ECF5FF;
+	display: flex;
+	justify-content: space-between;
+	padding: 110rpx 34rpx;
+	height: 490rpx;
+	background: rgba(0,0,0,0.4);
+	&-back{
+		width: 20rpx;
+		height: 36rpx;
+	}
+	&-fx{
+		width: 72rpx;
+		height: 72rpx;
+	}
+}
+.dynamic-main{
+	height: 620rpx;
+	background: #FFFFFF;
+	border-radius: 40rpx 40rpx 0rpx 0rpx;
+	margin-top: -306rpx;	
+	padding: 0 34rpx;
+}
+.main-top{
+	display: flex;
+	position: relative;
+	.header-img{
+		position: relative;
+		top: -74rpx;
+		width: 194rpx;
+		margin-right: 44rpx;
+		.headerimg{
+			width: 194rpx;
+			height: 194rpx;
+			background: #FFFFFF;
+			padding: 8rpx;
+			border-radius: 50%;
+		}
+		.header-auth{
+			width:56rpx;
+			height: 56rpx;
+			position: absolute;
+			bottom: 20rpx;
+			right: 5rpx;
+		}
+	}
+	.main-top-item{
+		margin-right: 46rpx;
+		.text{
+			font-size: 24rpx;
+			font-family: PingFangSC-Regular, PingFang SC;
+			font-weight: 400;
+			color: #888C90;
+			margin: 20rpx 0 9rpx 0;
+		}
+		.num{
+			font-size: 30rpx;
+			font-family: PingFang-SC-Heavy, PingFang-SC;
+			font-weight: 800;
+			color: #333333;
+		}
+	}
+	.gz{
+		position: absolute;
+		top: 30rpx;
+		right: 34rpx;
+		width: 190rpx;
+		height: 68rpx;
+		line-height: 68rpx;
+		background: #F3F3F3;
+		text-align: center;
+		border-radius: 34rpx;
+		font-size: 24rpx;
+		font-family: PingFangSC-Medium, PingFang SC;
+		font-weight: 500;
+		color: #888C90;
+	}
+}
+.person-name{
+	font-size: 40rpx;
+	font-family: PingFangSC-Semibold, PingFang SC;
+	font-weight: 600;
+	color: #1B1B1B;
+	line-height: 40rpx;
+	margin-top: 40rpx;
+	margin-bottom: 26rpx;
+}
+.lable{
+	display: flex;
+	view{
+		width: 84rpx;
+		height: 42rpx;
+		background: #EFF2FF;
+		border-radius: 21px;
+		margin-right: 30rpx;
+		font-size: 24rpx;
+		font-family: PingFangSC-Regular, PingFang SC;
+		font-weight: 400;
+		color: #676A8B;
+		line-height: 42rpx;
+		margin-bottom: 20rpx;
+		text-align: center;
+	}
+}
+</style>
