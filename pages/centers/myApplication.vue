00eaa4fd670f29adf8a958f2d3f37deff7c99c41
nibote <851080120@qq.com>
2022-03-08 18:30:33 +0800

    ''

--- pages/centers/myApplication.vue
+++ pages/centers/myApplication.vue
@@ -1,6 +1,10 @@
 <template>
 	<view class="myApplication">
-		我的报考
+		<view class="navbar">
+			<u-navbar title="专业评画" immersive back-icon-color="#ffffff" :background="background" :border-bottom="false"
+				title-color="#ffffff">
+			</u-navbar>
+		</view>
 	</view>
 </template>
 
@@ -8,15 +12,21 @@
 	export default {
 		data() {
 			return {
-				
+
 			}
 		},
 		methods: {
-			
+
 		}
 	}
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+	.myApplication {
+		.navbar {
+			height: 206rpx;
+			background-image: url('https://ykh-wxapp.oss-cn-hangzhou.aliyuncs.com/wx_applet_img/top_navbar_bg.png');
+			background-size: cover;
+		}
+	}
 </style>
